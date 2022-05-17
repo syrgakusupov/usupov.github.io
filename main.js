1515793c84f84bf118f45d52424d408a13d1a616
melisbekovkutman <melisbekovkutman@gmail.com>
2022-05-17 19:23:32 +0600

    text

---
+++ main.js
@@ -0,0 +1,89 @@
+// let name = 'usupov'
+// prompt(name)
+// const a = 10
+// const b = 15
+// console.log (a + b)
+// console.log (a * b)
+// console.log (b - a)
+// document.write(a / b)
+
+// if (s === (three){
+//     console.log('курс уже готов  и его можнот приходить')
+// } 
+// let ret ={
+//     name:'syrgak',
+//     age:20,
+//     hobbies:{
+//         play: 'soccer',
+//         loves:'play volleyball'
+//     }
+// }
+// console.log(ret)
+// console.log(Object.keys(ret))
+// console.log(Object.values(ret))
+
+
+// function syncho(a,b,c){
+//     alert(a+b+c)
+//     console.log(a+b+c)
+//     document.write(a+b+c)
+// }
+// syncho(10,10,10)
+
+// let nan = [2,4,5,6,8,9]
+// function name(nan){
+//     console.log(nan)
+//     document.write(nan)
+//     alert(nan)
+// }
+// name(nan)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let syncho = document.getElementById('btn')
+// syncho.addEventListener('click',usupov)
+// function usupov(){
+//     alert('krassavchik')
+// }
+// usupov()
+
+
+// let doc = document.querySelector('.bt')
+// doc.addEventListener('mousemove',usupov)
+// function usupov(){
+//     alert('krassavchik')
+// }
+// let ser =document.getElementById('home')
+// ser.addEventListener('click', hom)
+// function hom(){
+//     alert('usupovsyrgak')
+// }
+// hom()
+let noc =document.querySelector('.bt')
+noc.addEventListener('click', st)
+let p = document.getElementById('srs')
+function st(){
+    p.style.background='red'
+    p.style.color='black'
+}
+let ser = document.getElementById('tem')
+ser.addEventListener('click', br)
+function br(){
+    p.style.background='black'
+    p.style.color='red'
+}
+let sts =document.getElementById('imd')
+sts.addEventListener('mousemove')
+function bt(){
+}
